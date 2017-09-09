7c07ce62dee3b3e7228b5a6daef02c6da1400783
bgpat <bgpat23@gmail.com>
2017-09-09 18:37:00 +0900

    Add template

---
+++ template/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <router-link to="/">root</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ template/src/root.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <transition appear>
+      <span>root</span>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+div {
+  color: blue;
+}
+
+.v-enter-active, .v-leave-active {
+  transition: opacity .5s ease;
+}
+
+.v-enter, .v-leave-to {
+  opacity: 0;
+}
+</style>
